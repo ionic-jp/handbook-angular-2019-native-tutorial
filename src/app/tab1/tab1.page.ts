9ab61a30326732f9741112a02f3a87f418e13e3a
榊原昌彦 <sakakibara@rdlabo.jp>
2019-09-04 16:16:28 +0900

    ステップ２「アプリにネイティブ機能をつけよう」 コアプラグイン「ローカル通知でユーザにお知らせしよう」

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Plugins } from '@capacitor/core';
 
 @Component({
   selector: 'app-tab1',
@@ -9,4 +10,17 @@ export class Tab1Page {
 
   constructor() {}
 
+  localNotification() {
+    Plugins.LocalNotifications.schedule({
+      notifications: [
+        {
+          id: 1,
+          title: 'ようこそ',
+          body: 'Ionic Frameworkへ',
+          schedule: { at: new Date(Date.now() + 1000 * 5) },
+          sound: 'normail'
+        }
+      ]
+    });
+  }
 }
